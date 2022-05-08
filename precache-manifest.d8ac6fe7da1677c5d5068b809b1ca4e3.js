cdbad5e7dcd0aedf310d06ef940014d7b5221f52
18uma <yamamoto.y.bj@m.titech.ac.jp>
2022-05-08 09:04:54 +0900

    Updates

---
+++ precache-manifest.d8ac6fe7da1677c5d5068b809b1ca4e3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea9fc5e4be8e5ae25bf00dd1adb5148",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9af876a0f1b886313ebe",
+    "url": "/static/css/main.015e39d6.chunk.css"
+  },
+  {
+    "revision": "0504d6d228af79f2acda",
+    "url": "/static/js/2.215ea28e.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.215ea28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af876a0f1b886313ebe",
+    "url": "/static/js/main.8637a13e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "6eabbe4bb8f8cb0015c8b198e502a2fa",
+    "url": "/static/media/ap.6eabbe4b.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "ca11d78071f9540b7cf1f66060760478",
+    "url": "/static/media/dance.ca11d780.png"
+  },
+  {
+    "revision": "6bc261927f2d22b42bc9a60baed0caba",
+    "url": "/static/media/profile_image.6bc26192.jpg"
+  },
+  {
+    "revision": "68116725705a1d3fc86e912d732a6df5",
+    "url": "/static/media/study.68116725.png"
+  },
+  {
+    "revision": "6d36f35ebf333359ca3c9f77f2a06398",
+    "url": "/static/media/tenmasa.6d36f35e.png"
+  },
+  {
+    "revision": "16272391e60b81d5880496473e85ea7c",
+    "url": "/static/media/tennis.16272391.png"
+  },
+  {
+    "revision": "83480c793aa236c5c2adc82198872fd1",
+    "url": "/static/media/vr.83480c79.png"
+  }
+]);
